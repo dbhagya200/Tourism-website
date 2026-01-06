2c8b2ea862130aa8b4b6ba79f431baa40a98ac2a
dbhagya200 <dilinibhagya53@gmail.com>
2026-01-07 02:37:44 +0530

    Configure for deployment

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,19 @@
 import { useEffect } from "react";
-import {HashRouter, Route, Routes, useLocation} from "react-router-dom";
+import {HashRouter, Route, Routes} from "react-router-dom";
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 
 // Import your components...
 import { DefaultLayout } from "./view/common/defaultLayout/DefaultLayout";
-export function ScrollToTop() {
-    const { pathname } = useLocation();
-
-    useEffect(() => {
-        // This forces the browser to jump to the top (0, 0)
-        // whenever the URL path changes.
-        window.scrollTo(0, 0);
-    }, [pathname]);
-
-    return null;
-}
+// export function ScrollToTop() {
+//     const { pathname } = useLocation();
+//
+//     useEffect(() => {
+//         window.scrollTo(0, 0);
+//     }, [pathname]);
+//
+//     return null;
+// }
 
 function App() {
     // Initialize animations
@@ -25,7 +23,7 @@ function App() {
 
     return (
         <HashRouter>
-            <ScrollToTop/>
+            {/*<ScrollToTop/>*/}
             <Routes>
                 {/* Parent Route: Applies Navbar & Footer to everything inside */}
                 <Route path="/*" element={<DefaultLayout />}>
