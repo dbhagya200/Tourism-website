77e4aff8bc62c506930b29542d3d02ded6a7510c
Dilini B Warnakulasooriya <dilinibhagya53@gmail.com>
2026-02-13 10:18:40 +0530

    remove contact sidebar

--- src/view/common/defaultLayout/DefaultLayout.tsx
+++ src/view/common/defaultLayout/DefaultLayout.tsx
@@ -1,7 +1,7 @@
 import { Navbar } from "../navbar/Navbar.tsx";
 import { Footer } from "../footer/Footer.tsx";
 import {Main} from "../main/Main.tsx";
-import SideContactBar from "../sideContactBar/SideContactBar.tsx";
+// import SideContactBar from "../sideContactBar/SideContactBar.tsx";
 
 export function DefaultLayout() {
     return (
